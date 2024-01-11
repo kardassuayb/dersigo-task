4ce742c933df5e6e68b5a67076229862c4e97e88
Şuayb Kardaş <127879512+kardassuayb@users.noreply.github.com>
2024-01-11 04:52:35 +0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ import dersigo from "../asset/images/dersigo.png";
 
 const HomePage = () => {
   return (
-    <div className="flex items-center h-full">
+    <div className="flex items-center xs:mt-10 md:mt-0">
       <Image
         src={dersigo}
         className="mx-auto rounded-sm"
